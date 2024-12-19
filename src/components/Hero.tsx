37efe98b82316508615bbc10f829f990d82ed289
Praveen Kumar Varkala <64854489+PraveenKumarVk@users.noreply.github.com>
2024-12-19 13:01:33 -0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -30,7 +30,7 @@ const Hero = () => {
               <Linkedin size={24} />
             </a>
             <a
-              href="src/components/Praveen-Varkala-Resume.pdf"
+              href="/Praveen-Varkala-Resume.pdf"
               target="_blank"
               rel="noopener noreferrer"
               className="inline-flex items-center px-4 py-2 bg-primary text-white rounded-lg hover:bg-primary/90 transition-colors"
